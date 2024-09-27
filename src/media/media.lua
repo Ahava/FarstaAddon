ef1bb2ac659ea25bfe086b7e7f1bd03e4aa3e923
Wanja Sipinski <wanja@sipinski.de>
2024-09-27 08:05:56 +0200

    add first font

---
+++ src/media/media.lua
@@ -0,0 +1,12 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+local koKR, ruRU, zhCN, zhTW, western =
+	LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU, LSM.LOCALE_BIT_zhCN, LSM.LOCALE_BIT_zhTW, LSM.LOCALE_BIT_western
+
+local MediaType_FONT = LSM.MediaType.FONT
+
+LSM:Register(
+	MediaType_FONT,
+	"GothamNarrowUltra",
+	[[Interface\Addons\FarstaAddon\src\media\fonts\GothamNarrowUltra.ttf]],
+	koKR + ruRU + zhCN + zhTW + western
+)
