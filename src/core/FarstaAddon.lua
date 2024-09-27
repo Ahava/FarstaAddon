bb30279156863dab96707c3a441ec35b4dcfc59e
Wanja Sipinski <wanja@sipinski.de>
2024-09-27 07:42:40 +0200

    packaging libs maybe?

--- src/core/FarstaAddon.lua
+++ src/core/FarstaAddon.lua
@@ -1,4 +1,20 @@
----@class FarstaAddon : AceAddon-3.0
+---@class FarstaAddon : AceAddon-3.0, AceConsole-3.0
 
-local FarstaAddon = LibStub("AceAddon-3.0"):NewAddon("FarstaAddon")
+local FarstaAddon = LibStub("AceAddon-3.0"):NewAddon("FarstaAddon", "AceConsole-3.0")
 _G.FarstaAddon = FarstaAddon
+
+function FarstaAddon:OnInitialize()
+	self:RegisterChatCommand("farsta", "FarstaSlashCommand")
+end
+
+function FarstaAddon:OnEnable() end
+
+function FarstaAddon:OnDisable() end
+
+function FarstaAddon:FarstaSlashCommand(msg)
+	if msg == "install" then
+		self:Print("installer will come")
+	else
+		self:Print("doesnt exist lol")
+	end
+end
